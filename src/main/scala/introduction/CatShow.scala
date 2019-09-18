d54d67edbf43b1edd05ecc48576b3fd6ca6c54bb
tofu511 <mokmok511@gmail.com>
2019-09-19 00:18:56 +0900

    add implicit value for cat class

--- src/main/scala/introduction/CatShow.scala
+++ src/main/scala/introduction/CatShow.scala
@@ -1,5 +1,14 @@
 package introduction
 
-object CatShow {
+import cats.Show
+import cats.instances.int._
+import cats.instances.string._
+import cats.syntax.show._
 
+object CatShow {
+  implicit val catShow: Show[Cat] = Show.show[Cat] {cat =>
+    val name = cat.name.show
+    val age = cat.age.show
+    val color = cat.color.show
+    s"$name is a $age year-old $color cat."}
 }
--- src/test/scala/introduction/ShowTest.scala
+++ src/test/scala/introduction/ShowTest.scala
@@ -1,5 +1,13 @@
 package introduction
 
-class ShowTest {
+import org.scalatest.FunSuite
+import cats.syntax.show._
+import introduction.CatShow._
 
+class ShowTest extends FunSuite {
+
+  test("CatShow") {
+    val res = Cat("tama", 2, "black").show
+    assert(res === "tama is a 2 year-old black cat.")
+  }
 }
