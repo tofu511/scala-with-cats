592f88b9bcb34fb36efc6c46709acc90eaf65466
tofu511 <mokmok511@gmail.com>
2019-09-20 11:49:04 +0900

    add Monoid

---
+++ src/main/scala/monoid_and_semigroup/Monoid.scala
@@ -0,0 +1,35 @@
+package monoid_and_semigroup
+
+import cats.syntax.monoid
+
+trait Semigroup[A] {
+  def combine(x: A, y: A): A
+}
+
+trait Monoid[A] extends Semigroup[A] {
+  def empty: A
+}
+
+object Monoid {
+  def apply[A](implicit m: Monoid[A]) = monoid
+
+  implicit val booleanAndMonoid: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = true
+    override def combine(x: Boolean, y: Boolean): Boolean = x && y
+  }
+
+  implicit val booleanOrMonoid: Monoid[Boolean] = new Monoid[Boolean] {
+    override def empty: Boolean = false
+    override def combine(x: Boolean, y: Boolean): Boolean = x || y
+  }
+
+  implicit def setUnionMonoid[A]: Monoid[Set[A]] = new Monoid[Set[A]] {
+    override def empty: Set[A] = Set.empty
+    override def combine(x: Set[A], y: Set[A]): Set[A] = x.union(y)
+  }
+
+  implicit def setIntersectMonoid[A]: Monoid[Set[A]] = new Monoid[Set[A]] {
+    override def empty: Set[A] = Set.empty
+    override def combine(x: Set[A], y: Set[A]): Set[A] = x.intersect(y)
+  }
+}
